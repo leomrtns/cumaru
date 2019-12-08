86a1274cca53b49cffe0c34109672a378a538070
Leo Martins <Leonardo.de-Oliveira-Martins@quadram.ac.uk>
2019-12-08 12:45:42 +0000

    last version with original files (I'll modify them soon)

---
+++ kalign/kalign.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2019-today  Leonardo de Oliveira Martins [ leomrtns at gmail.com;  http://www.leomartins.org ]
+ * This file is part of cumaru, a multiple sequence alignment.
+ */
+
+/*! \file
+ *  \brief library based on [kalign3 multiple sequence alignment program](https://github.com/TimoLassmann/kalign.git)
+ */
+
+#ifndef _cumaru_kalign_h
+#define _cumaru_kalign_h
+
+#include "align_io.h"
+#include "alignment.h"
+#include "alignment_parameters.h"
+#include "aln_data.h"
+#include "alphabet.h"
+#include "bisectingKmeans.h"
+#include "bpm.h"
+#include "estimate_aln_param.h"
+#include "euclidean_dist.h"
+#include "global.h"
+#include "kmeans.h"
+#include "misc.h"
+#include "msa.h"
+#include "parameters.h"
+#include "pick_anchor.h"
+#include "rng.h"
+#include "sequence_distance.h"
+#include "tldevel.h"
+#include "weave_alignment.h"
+
+#endif
